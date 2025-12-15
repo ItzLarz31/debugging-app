81b6de4dd074d2ca29a7590e24dcc6f8e6bbba64
Shawn Dishman <97856123+ItzLarz31@users.noreply.github.com>
2025-12-15 11:01:04 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Please enter your name: ");
+string name = Console.ReadLine();
+Console.WriteLine($"Hello, {name}!");
+Console.Write($"Please enter your age: ");
+string ageInput = Console.ReadLine();
+
+try
+{
+    int age = int.Parse(ageInput);
+    double marsAge = age / 1.88;
+    Console.WriteLine($"You are {marsAge:F0} years old on Mars! How cool is that?");
+}
+catch (FormatException err)
+{
+    Console.WriteLine($"Please enter in a number: {err.Message}");
+}
